9bce3c20161b8022f511afda9b54cdbfedd9dca0
ridernagaraj <54629236+ridernagaraj@users.noreply.github.com>
2022-05-10 10:58:37 +0530

    Add files via upload

---
+++ twowheeler.java
@@ -0,0 +1,24 @@
+package week1day1;
+
+public class twowheeler {
+	int noOfWheels= 2;
+	short noOfMirrors=2;
+	long chassisNumber=157667796677l;
+	boolean isPunchured=true;
+	String bikeName= "Pulsar";
+	double runningKM=2000.1;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     twowheeler object= new twowheeler();
+     System.out.println("noOfWheels"+object.noOfWheels);
+     System.out.println(object.noOfMirrors);
+     System.out.println(object.chassisNumber);
+     System.out.println(object.isPunchured);
+     System.out.println(object.bikeName);
+     System.out.println(object.runningKM);
+     
+     
+	}
+	
+}
